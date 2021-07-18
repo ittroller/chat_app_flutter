5e207924540cba01f03a44745d5a510108922c39
Cao Kha Minh <ittroller8@gmail.com>
2021-07-18 23:49:00 +0700

    first commit

---
+++ lib/screens/chat_screen.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class ChatScreen extends StatelessWidget {
+  const ChatScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: StreamBuilder<QuerySnapshot>(
+        stream: FirebaseFirestore.instance
+            .collection('chats/ectVBoIS88rXoBXdjajI/messages')
+            .snapshots(),
+        builder: (ctx, streamSnapshot) {
+          if (streamSnapshot.hasError) {
+            return Center(
+              child: Text(streamSnapshot.error.toString()),
+            );
+          }
+
+          if (!streamSnapshot.hasData) {
+            return const Center(child: CircularProgressIndicator());
+          }
+
+          if (streamSnapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          }
+
+          final documents = (streamSnapshot.data!).docs;
+          return ListView.builder(
+            itemCount: documents.length,
+            itemBuilder: (ctx, index) => Container(
+              padding: EdgeInsets.all(8),
+              child: Text(documents[index]['text']),
+            ),
+          );
+        },
+      ),
+      floatingActionButton:
+          FloatingActionButton(child: Icon(Icons.add), onPressed: () {}),
+    );
+  }
+}
